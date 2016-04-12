9ec5b2f19dca8248708a01da9a083dc25f7adf1a
walifSub <walif.corp@gmail.com>
2016-04-12 23:07:19 +0600

    Add Upload Ashx file into the root directory

---
+++ CountryCityInformationManagement/CountryCityInformationManagement/Upload.ashx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CountryCityInformationManagement
+{
+    /// <summary>
+    /// Summary description for Upload
+    /// </summary>
+    public class Upload : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpPostedFile uploads = context.Request.Files["upload"];
+            string CKEditorFuncNum = context.Request["CKEditorFuncNum"];
+            string file = System.IO.Path.GetFileName(uploads.FileName);
+            uploads.SaveAs(context.Server.MapPath(".") + "\\images\\" + file);
+            //provide direct URL here
+            string url = "http://localhost:20368/images/" + file;
+
+            context.Response.Write("<script>window.parent.CKEDITOR.tools.callFunction(" + CKEditorFuncNum + ", \"" + url + "\");</script>");
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
